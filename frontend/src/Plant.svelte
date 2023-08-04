50619ebb8fb8f588b3791bb706d03e3edbe3c887
Julian Büttner <git@julianbuettner.dev>
2023-08-04 13:59:33 +0200

    Add plant component

---
+++ frontend/src/Plant.svelte
@@ -0,0 +1,21 @@
+<script>
+        export let id;
+        export let name;
+        export let amountMl;
+</script>
+
+<div>
+         <div>
+                <img
+                        src="https://watchandlearn.scholastic.com/content/dam/classroom-magazines/watchandlearn/videos/animals-and-plants/plants/what-are-plants-/english/wall-2018-whatareplantsmp4.transform/content-tile-large/image.png" alt="Image of plant"
+                        width=300
+                        height=300
+                        style="object-fit: cover"
+                >
+
+         </div>
+         <div>
+                {name}
+         </div>
+         Plant {name} gets {amountMl} daily
+ </div>
--- frontend/src/routes/+page.svelte
+++ frontend/src/routes/+page.svelte
@@ -1,2 +1,8 @@
+<script>
+        import Plant from '../Plant.svelte';
+</script>
+
+
 <h1>Welcome to SvelteKit</h1>
 <p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<Plant id=5 name="Bazel" amountMl="500"/>
